27a26ce3d4f44cee8246958fbad5c198060c37e3
joepadde <skildpadde.official@gmail.com>
2016-04-07 17:13:48 +0200

    Employee + Week were added. Class files of related objects were created as well.

---
+++ SE1/src/Classes/Employee.java
@@ -0,0 +1,67 @@
+package Classes;
+
+import java.util.List;
+
+public class Employee {
+	
+	private String initials;
+	public List<Project> projectList;
+	public List<Activity> activityList;
+	public List<double[]> workHourList;
+	
+	public Employee(String initials){
+		this.initials = initials;
+	}
+	
+	public String getInitials() {
+		return initials;
+	}
+	
+	public boolean assignProject(Project p) {
+		if (p != null && !projectList.contains(p)) {
+			projectList.add(p);
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean assignActivity(Activity a) {
+		if (a != null && !activityList.contains(a)) {
+			activityList.add(a);
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean setHours(Activity a, double hours, Week w, int weekday) {
+		if (a != null && hours > 0 && w.getWeek() > 0 && w.getWeek() <= 53 && weekday >= 1 && weekday <= 7) {
+			// set hours
+			return true;
+		}
+		return false;
+	}
+	
+	public double getWeeklyHours(Week w) {
+		double count = 0;
+		for (int i = 0; i < 7; i++) {
+			// iterate through weekdays
+		}
+		return count;
+	}
+	
+	public double getWorkHours(Activity a, Week w) {
+		double count = 0;
+		for (int i = 0; i < 7; i++) {
+			// iterate through weekdays
+		}
+		return count;
+	}
+	
+	public List<Activity> getWeeklyActivities(Week w) {
+		List<Activity> list = new List<Activity>();
+		return list;
+	}
+	
+}
+
+
---
+++ SE1/src/Classes/Week.java
@@ -0,0 +1,20 @@
+package Classes;
+
+public class Week {
+	private int year;
+	private int week;
+	
+	public Week(int year, int week) {
+		this.year = year;
+		this.week = week;
+	}
+	
+	public int getWeek() {
+		return week;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+	
+}
