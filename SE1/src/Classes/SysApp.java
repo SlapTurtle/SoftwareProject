f90100ebc1cf492e737a891fc0a1e4b6ba2da757
SlapTurtle <michaeldkirkegaard@gmail.com>
2016-04-07 14:09:32 +0200

    Test1

--- SE1/src/Classes/SysApp.java
+++ SE1/src/Classes/SysApp.java
@@ -4,5 +4,6 @@ public class SysApp {
 	public static void main(String[] args) {
 		//JEG ER TOM!
 		System.out.println("I HAVE POWER !!!");
+		System.out.println("MICHAEL HAS PWOERERER!!!!!");
 	}
 }
