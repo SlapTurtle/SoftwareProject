0096fd82d9169d72e454b7f2fefef64f6689509b
Benjamin <Benja.e95@gmail.com>
2016-04-12 11:43:32 +0200

    Activity testing added

---
+++ SE1/src/Tests/TestActivity.java
@@ -0,0 +1,18 @@
+package Tests;
+
+import Classes.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestActivity {
+	
+	
+		@Test
+		public void Activity() {
+			
+			
+		project.Activity econ = new project.Activity("Economy");
+		
+		
+		}
+}
